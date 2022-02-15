b97d8f465169f6073f5dfb892e418bcd3c0ae5f0
Vinicius Carmo <95287300+vmcarmo@users.noreply.github.com>
2022-02-15 14:06:37 -0300

    Add files via upload

---
+++ DataTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var data = DateTime.Now;
+
+            var formatada = String.Format("{0: dd/MM/yyyy hh:mm:ss z}", data);
+            Console.WriteLine(formatada);
+        }
+    }
+}
